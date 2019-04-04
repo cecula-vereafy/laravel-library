037ec96216d312ba0f62ccb964f0b4d04cc05971
Cecula Lab <CLab@localhost.localdomain>
2019-04-04 15:17:53 +0100

    Added example files for integration

---
+++ balance_laravel.php
@@ -0,0 +1,23 @@
+<?php 
+require 'vendor/autoload.php'; //Require php autoload
+use Illuminate\Guzzlehttp\guzzle;
+use GuzzleHttp\Client;    // Initialize Guzzle client
+use GuzzleHttp\Psr7\Request;
+GuzzleHttp\RequestOptions::DEBUG;
+
+
+
+$client = new \GuzzleHttp\Client();
+
+$method = 'GET';
+$url = 'https://api.cecula.com/account/tfabalance';
+$header = Array(
+    'Content-Type' => 'application/json',
+    'authorization' => 'Bearer' //Paste your key here
+);
+
+$response = $client->request($method, $url, [
+    'headers'        => $header,
+]);
+
+    echo $response->getBody();
---
+++ resend_laravel.php
@@ -0,0 +1,26 @@
+<?php 
+require 'vendor/autoload.php'; //Require php autoload
+use Illuminate\Guzzlehttp\guzzle;
+use GuzzleHttp\Client;    // Initialize Guzzle client
+use GuzzleHttp\Psr7\Request;
+
+$client = new \GuzzleHttp\Client();
+
+$method = 'POST';
+$url = 'https://api.cecula.com/twofactor/resend';
+$header = Array(
+    'Content-Type' => 'application/json',
+    'authorization' => 'Bearer' // Paste your key here
+);
+$body = Array( 
+    'pinRef' => '',
+    'mobile' => ''
+);
+
+
+$response = $client->request($method, $url, [
+    'headers'        => $header,
+    'json'  => $body,
+]);
+
+    echo $response->getBody();
